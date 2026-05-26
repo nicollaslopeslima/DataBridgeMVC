df00a0844a172e29875b08fc24db32e5014e4530
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-05-25 21:49:26 -0300

    feat: add SqlServerContext and MySqlContext

---
+++ Data/MySqlContext.cs
@@ -0,0 +1,23 @@
+﻿using DataBridge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBridge.Data
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<ClienteNovo> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClienteNovo>(entity =>
+            {
+                entity.ToTable("clientes");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.DataAlteracao)
+                      .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+        }
+    }
+}
---
+++ Data/SqlServerContext.cs
@@ -0,0 +1,26 @@
+﻿using DataBridge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBridge.Data
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options)
+            : base(options) { }
+
+        public DbSet<ClienteAntigo> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ClienteAntigo>(entity =>
+            {
+                entity.ToTable("Clientes");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.DataCadastro).
+                                HasDefaultValueSql("GETDATE()");
+                    
+            });
+        }
+    }
+}
