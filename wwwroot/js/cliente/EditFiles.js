eed8a4df7e6db968820090a3330d07a1141bca20
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-06-06 15:13:30 -0300

    feat: add EditFiles view

---
+++ wwwroot/js/cliente/EditFiles.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    // CEP automático
+    $('#cep').on('blur', async function () {
+        const cep = $(this).val().replace('-', '').trim();
+
+        if (cep.length !== 8) return;
+
+        const response = await fetch(`/Cliente/GetEnderecoPorCep?cep=${cep}`);
+
+        if (!response.ok) {
+            alert('CEP não encontrado.');
+            return;
+        }
+
+        const data = await response.json();
+        $('#endereco').val(data.logradouro);
+        $('#cidade').val(data.localidade);
+        $('#estado').val(data.uf);
+    });
+
+});
